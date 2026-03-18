a1fcdcabc0e2a6fd268eb8c0291155cb73a01987
RafaelCostaFreitas <freitas.rafael1@aluno.ifsp.edu.br>
2026-03-18 15:43:21 -0300

    implantando o service 57:00

--- app/src/main/java/br/edu/ifsp/scl/sdm/currencyconverter/model/api/CurrencyConverterApiClient.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/currencyconverter/model/api/CurrencyConverterApiClient.kt
@@ -1,5 +1,17 @@
 package br.edu.ifsp.scl.sdm.currencyconverter.model.api
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object CurrencyConverterApiClient {
-    private
+    private const val BASE_URL = "https://currency-converter5.p.rapidapi.com/currency/"
+
+    private val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(
+        GsonConverterFactory.create()
+    ).build() //esse objeto vai acessar o servico apartir da interface que criamos anteriormente 32:00
+
+    val service: CurrencyConverterApiService = retrofit.create(CurrencyConverterApiService::class.java) // classe onde esta descrito o acesso ao web
+
+
+
 }
