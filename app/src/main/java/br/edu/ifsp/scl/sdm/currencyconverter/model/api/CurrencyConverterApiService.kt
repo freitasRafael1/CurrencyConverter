8dff2b72000a307349f1f01ef52f1ec226be1407
RafaelCostaFreitas <freitas.rafael1@aluno.ifsp.edu.br>
2026-03-23 06:51:52 -0300

    final

--- app/src/main/java/br/edu/ifsp/scl/sdm/currencyconverter/model/api/CurrencyConverterApiService.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/currencyconverter/model/api/CurrencyConverterApiService.kt
@@ -7,6 +7,7 @@ import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
 
+/*
 interface CurrencyConverterApiService {
 
 
@@ -25,4 +26,27 @@ interface CurrencyConverterApiService {
         @Query("amount") amount: String
     ): Call<ConversionResult>
 
+}*/
+
+// ... (imports)
+
+interface CurrencyConverterApiService {
+
+    @Headers(
+        "x-rapidapi-host: currency-converter5.p.rapidapi.com",
+        "x-rapidapi-key: 92cbfcadbbmsh65c88133f386207p1389bfjsnc6224183e3af"
+    )
+    @GET("list")
+    fun getCurrencyList(): Call<CurrencyList>
+
+    @Headers(
+        "x-rapidapi-host: currency-converter5.p.rapidapi.com",
+        "x-rapidapi-key: 92cbfcadbbmsh65c88133f386207p1389bfjsnc6224183e3af"
+    )
+    @GET("convert")
+    fun convert(
+        @Query("from") from: String, //mapeando os parametros 22:40
+        @Query("to") to: String,
+        @Query("amount") amount: String
+    ): Call<ConversionResult>
 }
